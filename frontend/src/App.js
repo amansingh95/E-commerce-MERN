cf21f86a69313c587d10113b9dad6daff19a769a
aman <amansingh95>
2020-12-09 00:18:05 +0530

    Home screen, Rating component, React router,Product detail screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 
 import React from 'react';
+import { BrowserRouter as Router ,Route} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+
+
 const App = ()=> {
   return (
-    <div>
-      <h1>hii</h1>
-    </div>
+    <Router>
+    <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+        
+      </main>
+    <Footer />
+    
+      
+    </Router>
   );
 }
 
